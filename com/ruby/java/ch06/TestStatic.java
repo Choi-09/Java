3a5828ae28fb6c995888f154dcd997ee044f36dd
vanessajchoi <vanessajchoi@gmail.com>
2022-09-26 10:05:00 +0900

    static

--- com/ruby/java/ch06/TestStatic.java
+++ com/ruby/java/ch06/TestStatic.java
@@ -2,9 +2,16 @@ package com.ruby.java.ch06;
 
 public class TestStatic {
 
+	static void show() {
+		System.out.println("Hello");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a[][] = new int[3][4];
+		show();
+		TestStatic.show(); //생략가능
+		
 	}
 
 }
