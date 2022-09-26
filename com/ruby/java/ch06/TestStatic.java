cf7380462ff61884cf83de7fcaa871eb21a86da7
vanessajchoi <vanessajchoi@gmail.com>
2022-09-26 10:34:25 +0900

    배열 출력

--- com/ruby/java/ch06/TestStatic.java
+++ com/ruby/java/ch06/TestStatic.java
@@ -2,15 +2,23 @@ package com.ruby.java.ch06;
 
 public class TestStatic {
 
-	static void show() {
-		System.out.println("Hello");
+	static void show(int a[][]) {
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j <a[0].length; j++) {
+				
+				System.out.println(a[i][j]);
+				System.out.println("\t");
+			}
+		System.out.println("\n");
+		}
 	}
+		
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[][] = new int[3][4];
-		show();
-		TestStatic.show(); //생략가능
+		int a[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+		show(a);
+		TestStatic.show(a); //생략가능
 		
 	}
 
